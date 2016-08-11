c41613bbef4a9793e93836043ca2c2c8f398b366
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-08-11 09:29:40 +0200

    Initial Commit

---
+++ Shake It Baby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shake It Baby
+//
+//  Created by Ignacio Lasaosa Sáez on 11/8/16.
+//  Copyright © 2016 Ignacio Lasaosa Sáez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
